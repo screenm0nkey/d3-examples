48713b090fffc27c98cc3efc5d23c17c3d54b6c7
Nick Lowman <Nick.Lowman@morganstanley.com>
2015-06-16 14:37:13 +0100

    Updates and new additions

--- public/examples/joins-updates/update-enter-exit.js
+++ public/examples/joins-updates/update-enter-exit.js
@@ -1,12 +1,12 @@
 var p = d3.select("body").selectAll("p")
     .data([4, 8, 15, 16, 23, 42])
 
-// Update… (by inserting the string we are updating the selection)
-p.text(String);
-
 // Enter… (append new items)
 p.enter().append("p")
     .text(String);
+
+// Update all 'P' items… (by inserting the string we are updating the selection)
+p.text(String);
 
 // Exit…
 p.exit().remove();
